29ffe5a179e5eb399a8024228cb4d6e2c2647b91
XAmbert13 <44335019+XAmbert13@users.noreply.github.com>
2018-10-21 13:37:02 +0530

    Add files via upload

---
+++ ap2(spoj).c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+long int ap(long int x,long int y,long int z)
+int main()
+
+{
+
+int t;
+long int a3,al3,sum,n,a,d;
+scanf("%ld",&t);
+while(t>0)
+{
+t--;
+scanf("%ld %ld %ld",&a3,&al3,&sum);
+n=2*s/(a3+al3);
+d=(al3-a3)/(n-5);
+a=t3-2*d;
+printf("%ld\n",n);
+ap(a,d,n);
+printf("\n");
+}
+return 0;
+}
+long int ap(long int x,long int y,long int z)
+{
+while(n>0)
+{
+printf("%ld ",x);
+x+=y;
+n--;
+}
